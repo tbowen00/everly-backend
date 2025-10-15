e89f4596c3ea6de0878a4a925c36a62568114d7b
Brooklyn Lanning <brooklynlanning@Mac.attlocal.net>
2025-10-15 14:21:59 -0500

    Add all models to init script

--- init_db.py
+++ init_db.py
@@ -3,6 +3,8 @@ from database.connection import Base, engine
 from models.contact import Contact
 from models.campaign import Campaign
 from models.outreach import Outreach
+from models.lead_discovery import LeadDiscovery
+from models.email_template import EmailTemplate
 
 print("Creating database tables...")
 Base.metadata.create_all(engine)
